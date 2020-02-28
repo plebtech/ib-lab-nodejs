0d9d8076588c998840d22125e8e50b4a96605931
Nick Mc <nbmc@protonmail.com>
2020-02-28 15:59:31 -0600

    completed through part 1, step 4.

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const fs = require('fs');
+
+const dataPath = path.join(__dirname, "../chirps.json");
+
+const HARD_CODED_CHIRPS = [
+    {
+        id: 1,
+        account: '@BillyBob',
+        date: 'Sun Nov 10 2011 9:05:10 GMT-0600 (Central Standard Time)',
+        content: 'YEEEHAWW',
+    },
+    {
+        id: 2,
+        account: '@trump',
+        date: 'Fri Jan 1 2013 02:55:51 GMT-0600 (Central Standard Time)',
+        content: 'covfefe',
+    },
+    {
+        id: 3,
+        account: '@qwertyuiop',
+        date: 'Tue May 16 2014 9:15:11 GMT-0600 (Central Standard Time)',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, hic!',
+    },
+    {
+        id: 4,
+        account: '@qwertyuiop',
+        date: 'Tue May 17 2014 9:15:11 GMT-0600 (Central Standard Time)',
+        content: 'spam Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, hic!',
+    },
+    {
+        id: 5,
+        account: '@qwertyuiop',
+        date: 'Tue May 18 2014 9:15:11 GMT-0600 (Central Standard Time)',
+        content: 'lmao more spam Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, hic!',
+    },
+]
+
+const initHCC = () => {
+    fs.writeFile(dataPath, JSON.stringify(HARD_CODED_CHIRPS), err => {
+        if (err) console.log(err);
+        console.log('written.');
+    });
+}
+
+initHCC();
